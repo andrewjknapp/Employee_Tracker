e946f259dedcdb1ffd82d68b71f9f5263aaa5c14
Armadillo <ajknapp2015@gmail.com>
2020-02-24 22:00:53 -0800

    Added functionality to displayAllEmployees()

--- db/index.js
+++ db/index.js
@@ -3,57 +3,80 @@ const mysql = require('mysql');
 
 function dbmake(connection) {
     let database = Object.assign({}, functions);
-    database.connection = connection.query;
+    database.connection = connection;
     return database;
 }
 
 const functions = {
     findAllEmployees() {
-        // this.connection.query('SELECT * FROM employee');
+       return this.connection.query(
+           `
+           SELECT 
+                e.first_name AS First,
+                e.last_name AS Last,
+                r.title AS Role,
+                d.name AS Department 
+           FROM employee e
+           INNER JOIN role r ON e.role_id = r.id 
+           INNER JOIN department d ON r.department_id = d.id
+           `);
     },
+
     findAllDepartments() {
         console.log("Hello");
     },
+
     findAllRoles() {
         console.log("Hello");
     },
+
     findAllPossibleManagers(employeeId) {
         console.log("Hello");
     },
+
     findAllEmployeesByDepartment (departmentId) {
         console.log("Hello");
     },
+
     findAllEmployeesByManager() {
         console.log("Hello");
     },
+
     createEmployee(employee) {
         console.log("Hello");
     },
+
     createDepartment(department) {
         console.log("Hello");
     },
+
     createRole(role) {
         console.log("Hello");
     },
+
     updateEmployeeRole() {
         console.log("Hello");
     },
+
     updateEmployeeManager() {
         console.log("Hello");
     },
+
     removeEmployee(employeeId) {
         console.log("Hello");
     },
+
     removeDepartment(departmentId) {
         console.log("Hello");
     },
+    
     removeRole(roleId) {
         console.log("Hello");
     }
 };
 
-let db = dbmake();
+let db = dbmake(connection);
 
 module.exports = db;
 
-db.removeEmployee(1);
+//console.log(db);
